2d9824b171fcceb2a5df06279abd6f0f36f2e862
FrostMiKu <i@frostmiku.com>
2026-03-05 22:42:29 +0800

    refactor: remove heartbeat tool (now daemon-level mechanism)

--- src/vtuber/tools/__init__.py
+++ src/vtuber/tools/__init__.py
@@ -1,5 +1,11 @@
 from vtuber.tools.schedule import schedule_create, schedule_list, schedule_cancel
-from vtuber.tools.heartbeat import heartbeat
-from vtuber.tools.memory import recall, memorize, forget
+from vtuber.tools.memory import memorize, recall, forget
 
-__all__ = ["schedule_create", "schedule_list", "schedule_cancel", "heartbeat", "recall", "memorize", "forget"]
+__all__ = [
+    "schedule_create",
+    "schedule_list",
+    "schedule_cancel",
+    "memorize",
+    "recall",
+    "forget",
+]
